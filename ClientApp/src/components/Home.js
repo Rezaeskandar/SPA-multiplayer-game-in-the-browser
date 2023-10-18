1a9c52b1afe91f6c488e43e3024fadb1d1b1cd6b
Md-Ruhul-Amin-Rony <adiar1234@yahoo.com>
2023-10-18 11:52:54 +0200

    bit spelling change on home.js

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -7,12 +7,13 @@ export class Home extends Component {
   render() {
     return (
         <div>
-            <h1>Hello, world!</h1>
-            <p>Welcome to Single Page Application</p>
-            <p>We have seleted Guessing game. We have worked frontend on React and backend in C#.</p>
-            <p>You need to guess 1 to 10. You will get 10 times attempts to to guess the right number.</p>
-            <p>One game you may earn 100 points. And every attempts will deducted 10 points.</p>
-            <p>We will develope more option in this project.</p>
+            <h1>Hello, Viewers!</h1>
+            <p>Welcome to our Single Page Application</p>
+            <p>We have created a "Guessing game". We have worked frontend on React and backend in C#.</p>
+            <p>To play the game you need to be registered on our appication and Select the "Game" from Navigation menu.You need to guess a number between 1 to 10. You will get 10 times attempts to to guess the right number.</p>
+            <p>In one game you have the opportunity to earn highest 100 points. And every fail attempts will deduct 10 points.</p>
+            <p style={{ fontWeight: 'bold', color: 'goldenrod' }}>We are working to develop this project further.</p>
+
         </div>
     );
   }
